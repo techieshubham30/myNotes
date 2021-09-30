c45a9f372490c0c5970dd2ed3bc8376007717f94
techieshubham30 <82032825+techieshubham30@users.noreply.github.com>
2021-09-30 18:59:38 +0530

    initial commit

--- js/app.js
+++ js/app.js
@@ -4,7 +4,13 @@ const cardText=document.querySelector('.card-text');
 addBtn.addEventListener("click",addNotes);
 
 function addNotes(){
-
+let element=`<div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+<div class="card-body">
+  <h5 class="card-title">Note</h5>
+  <p class="card-text"></p>
+  <a href="#" class="btn btn-primary">Delete Note</a>
+</div>
+</div>`
 cardText.innerText=txtArea.value;
 txtArea.value="";
 }
