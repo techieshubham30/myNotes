d966143d04f65b5c2dbfb238a23c61375b937b84
techieshubham30 <82032825+techieshubham30@users.noreply.github.com>
2021-10-01 00:28:42 +0530

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,16 +1,49 @@
-const addBtn=document.querySelector('#addBtn');
-const txtArea=document.querySelector('#addTxt');
-const cardText=document.querySelector('.card-text');
-addBtn.addEventListener("click",addNotes);
+// If user adds a note, add it to the localStorage
+showNotes();
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function (e) {
+  let addTxt = document.getElementById("addTxt");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } 
+  else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  console.log(notesObj);
+  showNotes();
+});
 
-function addNotes(){
-let element=`<div class="noteCard my-2 mx-2 card" style="width: 18rem;">
-<div class="card-body">
-  <h5 class="card-title">Note</h5>
-  <p class="card-text"></p>
-  <a href="#" class="btn btn-primary">Delete Note</a>
-</div>
-</div>`
-cardText.innerText=txtArea.value;
-txtArea.value="";
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } 
+  else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function (element, index) {
+    html += `
+   <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Note ${index+1}</h5>
+                  <p class="card-text">${element}</p>
+                  <button id="${index}" onclick="deleteNote(this,id)" class="btn btn-primary">Delete Note</button>
+                </div>
+              </div>`;
+  });
+
+  let notesElm=document.getElementById('notes');
+  if(notesObj.length!=0){
+    notesElm.innerHTML=html;
+  }
+
+}
+
+function deleteNote(index){
+  console.log("delete");
 }
